961f2fcfd39ea63188192766890c97d2d47056cf
Aleksey Proshutinskiy <aleksey.proshutinskiy@emlid.com>
2018-07-27 17:53:00 +0300

    src: rtksvr.c: send solution status to NTRIP
    In NMEA GGA

--- src/rtksvr.c
+++ src/rtksvr.c
@@ -486,7 +486,7 @@ static void send_nmea(rtksvr_t *svr, unsigned int *tickreset)
     }
     else if (svr->nmeareq == NMEAREQ_SEND_SINGLE) { /* single-solution mode */
         if (norm(svr->rtk.sol.rr,3)<=0.0) return;
-        sol_nmea.stat=SOLQ_SINGLE;
+        sol_nmea.stat=svr->rtk.sol.stat;
         sol_nmea.time=utc2gpst(timeget());
         sol_nmea.ns = svr->rtk.sol.ns;
         matcpy(sol_nmea.rr,svr->rtk.sol.rr,3,1);
